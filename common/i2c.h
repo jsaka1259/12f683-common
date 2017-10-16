ebc41e3148fdb8a5f1b8fb46b4477a0cc631b59a
jsaka1259 <jsaka1259@gmail.com>
2017-10-16 20:33:06 +0900

    Add extern

--- common/i2c.h
+++ common/i2c.h
@@ -1,10 +1,10 @@
 #ifndef I2C_H
 #define	I2C_H
 
-void i2c_start(void);
-void i2c_stop(void);
-uint8_t i2c_write(uint8_t data);
-uint8_t i2c_read(uint8_t ack);
+extern void i2c_start(void);
+extern void i2c_stop(void);
+extern uint8_t i2c_write(uint8_t data);
+extern uint8_t i2c_read(uint8_t ack);
 
 #endif	/* I2C_H */
 
--- common/spi.h
+++ common/spi.h
@@ -1,10 +1,10 @@
 #ifndef SPI_H
 #define SPI_H
 
-void spi_start(void);
-void spi_end(void);
-void spi_out(unsigned char data);
-uint8_t spi_rcv(void);
-uint8_t spi_rcv16(void);
+extern void spi_start(void);
+extern void spi_end(void);
+extern void spi_out(unsigned char data);
+extern uint8_t spi_rcv(void);
+extern uint8_t spi_rcv16(void);
 
 #endif	/* SPI_H */
